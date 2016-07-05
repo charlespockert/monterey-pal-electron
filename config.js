fe56568755ff055d4cb6abd6222b056a1494fa5a
Jeroen <Jeroenvinke17@gmail.com>
2016-07-05 08:24:07 +0200

    chore(version): version bump

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@0.9.18",
-    "monterey-pal": "github:monterey-pal@0.1.3",
+    "monterey-pal": "github:monterey-pal@0.1.4",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
