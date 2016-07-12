f6fb830688ee6b4ba39f1ac4846e6c6fbba575f2
charles.pockert@googlemail.com <charles.pockert@googlemail.com>
2016-07-12 23:24:28 +0100

    feat(pal): added openItem implementation

--- src/os.js
+++ src/os.js
@@ -1,7 +1,12 @@
 const os = System._nodeRequire('os');
+const shell = System._nodeRequire('electron').shell;
 
 export class OS {
   getPlatform() {
     return os.platform();
+  }
+
+  openItem(path) {
+    shell.openItem(path);
   }
 }
